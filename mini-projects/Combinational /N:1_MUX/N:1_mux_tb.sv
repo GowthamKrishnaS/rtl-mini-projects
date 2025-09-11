e1b72a51e8bc24a45870c4dc1c8a917e15889f2f
GowthamKrishnaS <gowthamkrishnasuresh1@gmail.com>
2025-09-11 16:12:28 +0530

    Update N:1_mux_tb.sv

--- mini-projects/Combinational /N:1_MUX/N:1_mux_tb.sv
+++ mini-projects/Combinational /N:1_MUX/N:1_mux_tb.sv
@@ -1,21 +1,24 @@
+
+
+
 `timescale 1ns/1ps
 module nto1_mux_tb;
-parameter width = 32 ;
-parameter N= 8;
-logic  [width-1:0] d[0:N-1];
-logic [$clog2(N)-1:0]sel;
-logic [width-1:0] m_out;
-nto1_mux #(.width(width), .N(N)) dut (.*);
-task automatic check_mux(input [$clog2(N)-1:0]s,input [width-1:0] expected);
-sel=s;
+    parameter width = 32 ;
+    parameter N= 8;
+    logic  [width-1:0] d[0:N-1];
+    logic [$clog2(N)-1:0]sel;
+    logic [width-1:0] m_out;
+    nto1_mux #(.width(width), .N(N)) dut (.*);
+        task automatic check_mux(input [$clog2(N)-1:0]s,input [width-1:0] expected);
+            sel=s;
 #1
-assert(m_out === expected)
-$display("pass: sel=%b | m_out=%h | expected=%h|",s,m_out,expected);
-else
-$error("failed: sel=%b | m_out=%h | expected=%h|",s,m_out,expected);
+        assert(m_out === expected)
+            $display("pass: sel=%b | m_out=%h | expected=%h|",s,m_out,expected);
+        else
+             $error("failed: sel=%b | m_out=%h | expected=%h|",s,m_out,expected);
 
-endtask 
-initial begin
+    endtask 
+    initial begin
         d[0] = 32'hAAAA_AAAA; 
         d[1]= 32'hBBBB_BBBB;
         d[2] = 32'hCCCC_CCCC;
@@ -25,7 +28,7 @@ initial begin
         d[6] = 32'h1111_1111;
         d[7] = 32'h2222_2222;
 
-for(int i=0;i<N;i++)begin
+    for(int i=0;i<N;i++)begin
         check_mux(i,d[i]);
      end
 $display("All testcases completed");
